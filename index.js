47d82b3fd0766a56d8ac4e03ac886081a9627f71
fazlehasan <fsdigitalgraphic@gmail.com>
2025-01-04 19:52:39 +0600

    3dr commit for NodejsProject

--- index.js
+++ index.js
@@ -10,10 +10,15 @@
 const http = require("http");
 const { hendleReqRes } = require("./helpers/hendleReqRes");
 const environment  = require('./helpers/enviroments');
+const data = require('./lib/data')
 
 // app object - module scaffolding
 const app = {};
 
+// for Test file create
+data.update('test','newFile', {fname:"Suraiya",lname:"Jahan",age:"23",city:"Dhaka"}, (err,result)=>{
+  console.log(err, result)
+})
 
 // create server
 app.createServer = () => {
