5d4a0992ec2448edb1a30464cc939f3b8ad37acd
fazlehasan <fsdigitalgraphic@gmail.com>
2025-01-06 01:11:40 +0600

    8th commit for NodejsProjectnp add file tokenhandler

--- routes.js
+++ routes.js
@@ -8,10 +8,12 @@
 // dependencies
 const { sampleHandler } = require("./handlers/routeHandler/sampleHandler");
 const { userHandler } = require("./handlers/routeHandler/userHandler");
+const { tokenHandler } = require("./handlers/routeHandler/tokenHandler");
 
 const routes = {
   sample: sampleHandler,
   user: userHandler,
+  token: tokenHandler,
 };
 
 module.exports = routes;
