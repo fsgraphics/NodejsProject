62c6429eda327fe83c8be2c27bb18efdcbbc33f6
fazlehasan <fsdigitalgraphic@gmail.com>
2025-01-05 20:59:31 +0600

    7th commit as NodejsProject

--- handlers/routeHandler/userHandler.js
+++ handlers/routeHandler/userHandler.js
@@ -8,6 +8,7 @@
 // dependencies
 const data = require("../../lib/data");
 const { hash } = require("../../helpers/utilities");
+const { parseJSON } = require("../../helpers/utilities");
 
 // module scaffolding
 
@@ -74,7 +75,7 @@ handler._users.post = (requestProperties, callback) => {
               message: "User was created successfully!",
             });
           } else {
-            callback(500, { error:"Could not create user!" });
+            callback(500, { error: "Could not create user!" });
           }
         });
       } else {
@@ -90,8 +91,97 @@ handler._users.post = (requestProperties, callback) => {
   }
 };
 
-handler._users.get = (requestProperties, callback) => {};
-handler._users.put = (requestProperties, callback) => {};
+handler._users.get = (requestProperties, callback) => {
+  // check the phone number if valid
+  const phone =
+    typeof requestProperties.queryStringObject.phone === "string" &&
+    requestProperties.queryStringObject.phone.trim().length === 11
+      ? requestProperties.queryStringObject.phone
+      : false;
+  if (phone) {
+    // lookup the user
+    data.read("users", phone, (err, u) => {
+      const user = { ...parseJSON(u) };
+      console.log(u);
+      if (!err && user) {
+        delete user.password;
+        callback(200, user);
+      } else {
+        callback(404, { error: "Request user was not found" });
+      }
+    });
+  } else {
+    callback(404, { error: "Request user was not foundfd" });
+  }
+};
+handler._users.put = (requestProperties, callback) => {
+  // check the phone number if valid
+  const phone =
+    typeof requestProperties.body.phone === "string" &&
+    requestProperties.body.phone.trim().length === 11
+      ? requestProperties.body.phone
+      : false;
+  const firstName =
+    typeof requestProperties.body.firstName === "string" &&
+    requestProperties.body.firstName.trim().length > 0
+      ? requestProperties.body.firstName
+      : false;
+
+  const lastName =
+    typeof requestProperties.body.lastName === "string" &&
+    requestProperties.body.lastName.trim().length > 0
+      ? requestProperties.body.lastName
+      : false;
+  const password =
+    typeof requestProperties.body.password === "string" &&
+    requestProperties.body.password.trim().length > 0
+      ? requestProperties.body.password
+      : false;
+
+  if (phone) {
+    if (firstName || lastName || password) {
+      // lookup the user
+      data.read("users", phone, (err, uData) => {
+        const userData = { ...parseJSON(uData) };
+        if (!err && uData) {
+          if (firstName) {
+            userData.firstName = firstName;
+          }
+          if (lastName) {
+            userData.lastName = lastName;
+          }
+          if (password) {
+            userData.password = hash(password);
+          }
+          // store to database
+          data.update("users", phone, userData, (err) => {
+            if (!err) {
+              callback(200, {
+                message: "User was updated successfully!",
+              });
+            } else {
+              callback(500, {
+                error: "There was a problem in the server side!",
+              });
+            }
+          });
+        } else {
+          callback(400, {
+            error: "You have a problem in your request!",
+          });
+        }
+      });
+    } else {
+      callback(400, {
+        error: "You have a problem in your request!",
+      });
+    }
+  } else {
+    callback(400, {
+      error: " Invalid phone number. Please try again!",
+    });
+  }
+};
 handler._users.delete = (requestProperties, callback) => {};
 
 module.exports = handler;
